80fc793f2fd037734772d8b3c692a8b0e2a81210
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-12-11 05:08:18 +0100

    add general setting fish control

--- src/components/Boids.jsx
+++ src/components/Boids.jsx
@@ -5,9 +5,23 @@ import { useEffect, useMemo, useRef } from "react";
 import { Vector3 } from "three";
 import { SkeletonUtils } from "three-stdlib";
 import { themeAtom, THEMES } from "./UI";
+import { useControls } from "leva";
 
 export const Boids = ({}) => {
   const [theme] = useAtom(themeAtom);
+  const { NB_BOIDS, MIN_SCALE, MAX_SCALE, MIN_SPEED, MAX_SPEED, MAX_STEERING } =
+    useControls(
+      "General settings",
+      {
+        NB_BOIDS: { value: 100, min: 1, max: 200 },
+        MIN_SCALE: { value: 0.7, min: 0.1, max: 2, step: 0.1 },
+        MAX_SCALE: { value: 1.3, min: 0.1, max: 2, step: 0.1 },
+        MIN_SPEED: { value: 0.9, min: 0, max: 10, step: 0.1 },
+        MAX_SPEED: { value: 3.6, min: 0, max: 10, step: 0.1 },
+        MAX_STEERING: { value: 0.1, min: 0, max: 1, step: 0.01 },
+      },
+      { collapsed: true }
+    );
   return (
     <>
       <Boid
